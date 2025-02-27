c46a64e1e3aec014f2966d665b8eac7b9cf2114d
Giulio <giuliopanza2000@gmail.com>
2025-02-27 17:02:09 +0100

    add delete

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,29 @@ function App() {
 
   const apiUrl = 'http://localhost:3000/api/posts';
 
+  const initialFormData = {
+    name: '',
+    image: '',
+    ingredients: [],
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
   function fetchPosts() {
     axios.get(apiUrl)
     .then((res) => setPosts(res.data))
     .catch(err => console.error(err) )
   }
+
+  const handleDelete = (id) => {
+
+    fetch( `${apiUrl}/${id}`, {
+      method: 'DELETE'
+    })
+    .then(
+     fetchPosts()
+    )
+  };
 
   useEffect(fetchPosts, []);
 
@@ -28,6 +46,11 @@ function App() {
                     <img src={elem.image} alt={elem.title} />
                   </figure>
                   <h3>{elem.title}</h3>
+                  <button
+                   className="btn btn-danger"
+                   onClick={() => handleDelete(elem.id)}>
+                   x
+                  </button>
                 </div>
               </div>
             );
